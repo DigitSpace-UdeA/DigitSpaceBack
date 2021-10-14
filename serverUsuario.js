350dc7be600f619233c749344c897a7b084de404
KaterinBarrera <katerinbarrera2001@gmail.com>
2021-10-14 01:07:10 -0500

    api

---
+++ serverUsuario.js
@@ -0,0 +1,66 @@
+//Hacer el import de express
+//const express = require('express');
+
+import Express  from "express";
+import { MongoClient } from "mongodb";
+
+const stringConexion =
+'mongodb+srv://KaterinB:DigitKaterin@proyectodigitspace.hfib8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+
+const client = new MongoClient(stringConexion, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+let conexion;
+
+
+const app = Express()
+app.use = Express.json();
+
+
+
+//creación de rutas
+app.get("/moduloUsuarios2", (req, res)=>{
+    console.log("alguien hizo get en ruta modulous");
+   conexion
+   .collection("Listado Usuarios")
+   .find({}) //filtros que uno quiera, se tiene que poner sí o sí
+   .limit(50) //limites, opcional
+   .toArray((err, result)=>{
+    if(err){
+        res.status(500).send("error en get");
+    }
+    else{
+        res.json(result);
+    }
+   })
+} );
+
+
+//app.post("/editarUsuaio2", (req, res) =>{
+   // console.log('solocitud post', req.body);
+   //rest.send(' post' creado);
+//})
+
+
+
+
+// escucha solicitudes en un puerto especifico
+
+
+const main = () => {
+    client.connect((err,db)=>{
+        if(err){
+            console.error("error conectar base de datos");
+            //return false;
+        }
+        conexion = db.db('Usuarios');
+        console.log('conexión exitosa');
+        return app.listen(5000, () =>{
+            console.log('escuchando puerto');
+        });
+    });
+};
+
+main()
