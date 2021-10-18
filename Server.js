286756fcdcfd9fd6bbff1dff608edab23c168c34
David <davico.gonzalez@live.com>
2021-10-18 16:03:39 -0500

    Creación API para POST de Ventas.

--- Server.js
+++ Server.js
@@ -1,9 +1,10 @@
 import Express from 'express';
 import { MongoClient } from 'mongodb';
 
-const stringConexion = ""
+const stringConexion =
+  "mongodb+srv://Tutor:DigitTutor@proyectodigitspace.hfib8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
-const client = MongoClient(stringConexion, {
+const client = new MongoClient(stringConexion, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
@@ -14,22 +15,52 @@ app.use(Express.json());
 
 app.get('/ventas', (req, res) => {
     console.log("alguien hizo get en la ruta /ventas");
-    const ventas = [
-      { nombre: "Sofa", marca: "Deko", precio: "2000", modelo: "2012" },
-      { nombre: "Cama", marca: "Deko", precio: "3500", modelo: "2014" },
-      { nombre: "Sofacama", marca: "Deko", precio: "1950", modelo: "2018" },
-      { nombre: "Silla Individual", marca: "Isabelina", precio: "850", modelo: "2020" },
-      { nombre: "Comedor", marca: "Deko", precio: "1700", modelo: "2019" },
-      { nombre: "Sala", marca: "Kingdom", precio: "4500", modelo: "2016" },
-    ];
+    conexion.collection("Ventas").find    
     res.send(ventas);
 });
 
-app.post("/ventas/nueva", (req, res) => {
-    console.log("Vehículo a crear: ", req.body);
-    res.send("Venta creada exitósamente");
+app.post('/ventas/nueva', (req, res) => {
+    console.log(req);
+    const datosVenta = req.body;
+    console.log("Llaves: ", Object.keys(datosVenta));
+    try {
+        if (
+            Object.keys(datosVenta).includes("name") &&
+            Object.keys(datosVenta).includes("brand") &&
+            Object.keys(datosVenta).includes("price")
+        ) {
+            conexion.collection("Ventas").insertOne(datosVenta, (err, result) => {
+                if (err) {
+                    console.log(err);
+                    res.sendStatus(500);
+                } else {
+                    console.log(result);
+                    res.sendStatus(200);
+                }
+            });
+            res.sendStatus(200);
+        } else {
+            res.sendStatus(500);
+        }
+    } catch {
+        res.sendStatus(500);
+    }
 });
 
-app.listen(5000, () => {
-    console.log("Escuchando puerto 5000");
-});
+let conexion;
+
+const main = () => {
+    
+    client.connect((err, db) => {
+        if (err) {
+            console.log("Error conectando a la base de datos");
+            return "error";
+        }
+        conexion = db.db("mobiliaria");
+        return app.listen(5000, () => {
+            console.log("Escuchando puerto 5000");
+        });
+    });
+};
+
+main();
