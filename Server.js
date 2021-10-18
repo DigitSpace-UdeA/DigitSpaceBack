b729289f28f2782051b1c9a9ae344c42df3257a3
David <davico.gonzalez@live.com>
2021-10-18 18:49:03 -0500

    Creación API para GET, PATCH y DELETE de Ventas.

--- Server.js
+++ Server.js
@@ -1,5 +1,6 @@
 import Express from 'express';
-import { MongoClient } from 'mongodb';
+import { MongoClient, ObjectId } from 'mongodb';
+import Cors from "cors";
 
 const stringConexion =
   "mongodb+srv://Tutor:DigitTutor@proyectodigitspace.hfib8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
@@ -10,13 +11,18 @@ const client = new MongoClient(stringConexion, {
 });
 
 const app = Express();
-
 app.use(Express.json());
+app.use(Cors());
 
 app.get('/ventas', (req, res) => {
-    console.log("alguien hizo get en la ruta /ventas");
-    conexion.collection("Ventas").find    
-    res.send(ventas);
+    console.log("Se consulta la base de datos que está en la ruta /ventas");
+    conexion.collection("Ventas").find({}).limit(50).toArray((err, result) => {
+        if (err) {
+            res.sendStatus(500).send("Error consultando las ventas.");
+        } else {
+            res.json(result);
+        }
+    });
 });
 
 app.post('/ventas/nueva', (req, res) => {
@@ -47,18 +53,57 @@ app.post('/ventas/nueva', (req, res) => {
     }
 });
 
+app.patch("/ventas/editar", (req, res) => {
+    const edicion = req.body;
+    console.log(edicion);
+    const filtroVenta = {_id: new ObjectId(edicion.id)};
+    delete edicion.id;
+    const operacion = {
+      $set: edicion,
+    };
+    conexion
+      .collection("Ventas")
+      .findOneAndUpdate(
+        filtroVenta,
+        operacion,
+        { upsert: true, returnOriginal: true },
+        (err, result) => {
+          if (err) {
+            console.error("Error actualizando la venta: ", err);
+            res.sendStatus(500);
+          } else {
+            console.log("Actualización exitosa.");
+            res.sendStatus(200);
+          }
+        }
+      );
+});
+
+app.delete("/ventas/eliminar", (req, res) => {
+    const filtroVenta = { _id: new ObjectId(req.body.id) };
+    conexion.collection("Ventas").deleteOne(filtroVenta, (err, result) => {
+        if (err) {
+            console.error(err);
+            res.sendStatus(500);
+        } else {
+            console.log("Venta eliminada exitosamente.");
+            res.sendStatus(200);
+        }      
+    });
+});
+
+
 let conexion;
 
-const main = () => {
-    
+const main = () => {    
     client.connect((err, db) => {
         if (err) {
             console.log("Error conectando a la base de datos");
-            return "error";
         }
         conexion = db.db("mobiliaria");
+        console.log("Conexión exitosa.");
         return app.listen(5000, () => {
-            console.log("Escuchando puerto 5000");
+            console.log("Escuchando puerto 5000.");
         });
     });
 };
